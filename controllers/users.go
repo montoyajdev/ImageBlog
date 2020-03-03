c8c92a2f16788d4598583ac745c070b57162a839
montoyajdev <montoyajdev@gmail.com>
2020-03-02 21:42:44 -0800

    moved signup to controllers

---
+++ controllers/users.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"net/http"
+
+	"lenslocked.com/views"
+)
+
+// NewUsers is used o create a new Users controller
+// This function wil panic  if the templates are not
+// parsed correctly, and should only be used during
+// initial setup.
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+type Users struct {
+	NewView *views.View
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
