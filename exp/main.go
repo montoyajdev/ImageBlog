fa8082f9473a4f4a8f06c411f8654be4bb8eb8d9
montoyajdev <montoyajdev@gmail.com>
2020-03-08 21:31:50 -0700

    Relational GORM

---
+++ exp/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "janesmith"
+	dbname = "lenslocked_dev"
+)
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+	Color string
+}
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := gorm.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	db.LogMode(true)
+	db.AutoMigrate(&User{})
+
+	var u User
+	db.First(&u)
+	fmt.Println(u)
+}
