0851c9ab60a898692f070e2832ec2dd18cc50539
montoyajdev <montoyajdev@gmail.com>
2020-03-16 01:20:30 -0700

    Implemented and created the UserDB interface

--- exp/main.go
+++ exp/main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/base64"
 	"fmt"
-
-	"lenslocked.com/hash"
 )
 
+type Dog struct{}
+
+func (d Dog) Speak() {
+	fmt.Println("woof")
+}
+
+type Husky struct {
+	Speaker
+}
+
+type Speaker interface {
+	Speak()
+}
+
 func main() {
-	toHash := []byte("this is my string to hash")
-
-	h := hmac.New(sha256.New, []byte("my-secret-key"))
-	h.Write(toHash)
-	b := h.Sum(nil)
-	fmt.Println(base64.URLEncoding.EncodeToString(b))
-
-	hmac := hash.NewHMAC("my-secret-key")
-	fmt.Println(hmac.Hash("this is my string to hash"))
-
+	h := Husky{Dog{}}
+	h.Speak()
 }
