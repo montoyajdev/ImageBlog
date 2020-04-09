61591172d3b7bc263b691061ef882f8b083b3e22
montoyajdev <montoyajdev@gmail.com>
2020-04-09 00:15:41 -0700

    Updated the gallery service to include an Update method and hooked this to the galleries update action

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -86,7 +86,17 @@ func (g *Galleries) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	gallery.Title = form.Title
-	fmt.Fprintln(w, gallery)
+	err = g.gs.Update(gallery)
+	if err != nil {
+		vd.SetAlert(err)
+		g.EditView.Render(w, vd)
+		return
+	}
+	vd.Alert = &views.Alert{
+		Level:   views.AlertLvlSuccess,
+		Message: "Gallery succesfully updated!",
+	}
+	g.EditView.Render(w, vd)
 }
 
 // POST /galleries
